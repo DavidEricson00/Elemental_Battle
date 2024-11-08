b1516dd0c5444c014964578b77062d39c26ebfd6
David Ericson <davidestudofcg@gmail.com>
2024-11-08 19:29:54 -0300

    feat(Battle): Turn switch added and better damage logic.

--- src/Battle.java
+++ src/Battle.java
@@ -1,32 +1,35 @@
 public class Battle {
-	
-	public void Fight(Element e1, Element e2) {
-		Element attacking = e1;
-		Element defending = e2;
-		Element aux;
-		
-		int dmg, def, impact, turn=1;
-		
-		do {
-			dmg = attacking.Attack();
-			def = defending.Defend();
-			impact = (dmg - def);
-			
-			if (dmg > def) {
-				System.out.println(attacking.getName() + " deals " + impact + " damege to " + defending.getName());
-				defending.Damage(impact);
-			}
-			else
-				System.out.println(defending.getName() + " deffended the attack ");
-			
-		} while (e1.getLife() > 0 && e2.getLife()> 0);
-		
-		if (e1.getLife() <= 0)
-			System.out.println(e2.getName() + " Won the battle!");
-		else
-			System.out.println(e1.getName() + " Won the battle!");
 
-	}
-	
+    public void Fight(Element e1, Element e2) {
+        Element attacking = e1;
+        Element defending = e2;
+        Element aux;
 
+        int dmg, def, impact, turn = 1;
+
+        do {
+        	System.out.println("Turn " + (turn++));
+            dmg = attacking.Attack();
+            def = defending.Defend();
+
+            if (def > dmg)
+            	System.out.println(defending.getName() + " defended the attack!");
+            else {
+            	impact = Math.abs(dmg - def);
+            	System.out.println(attacking.getName() + " deals " + impact + " damage to " + defending.getName());
+            	defending.Damage(impact);
+            }
+
+            aux = attacking;
+            attacking = defending;
+            defending = aux;
+
+        } while (e1.getLife() > 0 && e2.getLife() > 0);
+
+        if (e1.getLife() <= 0) {
+            System.out.println(e2.getName() + " won the battle!");
+        } else {
+            System.out.println(e1.getName() + " won the battle!");
+        }
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		Battle battle = new Battle();
 		
-		Element e1 = new Element("Aqua","Water",1000,30,60);
-		Element e2 = new Element("Flame","Water",1000,60,30);
+		Element e1 = new Element("Aqua","Water",100,30,60);
+		Element e2 = new Element("Flame","Fire",100,60,30);
 	
 		battle.Fight(e1, e2);
 	}
