321b7ea57a0c726f096b6d8e1f42b40cedde3d88
David Ericson <davidestudofcg@gmail.com>
2024-11-08 19:36:09 -0300

    feat(Battle): print elements stats during battle.

--- src/Battle.java
+++ src/Battle.java
@@ -8,10 +8,14 @@ public class Battle {
         int dmg, def, impact, turn = 1;
 
         do {
+            System.out.println("---------------------------------------");
         	System.out.println("Turn " + (turn++));
             dmg = attacking.Attack();
             def = defending.Defend();
-
+            
+            System.out.println(attacking.getName() + " Attacking with: " + dmg);
+            System.out.println(defending.getName() + " Defending with: " + def);
+            
             if (def > dmg)
             	System.out.println(defending.getName() + " defended the attack!");
             else {
@@ -23,6 +27,9 @@ public class Battle {
             aux = attacking;
             attacking = defending;
             defending = aux;
+            
+            System.out.println(e1.getName() + " Life: " + e1.getLife());
+            System.out.println(e2.getName() + " Life: " + e2.getLife());
 
         } while (e1.getLife() > 0 && e2.getLife() > 0);
 
