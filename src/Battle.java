62f7cb163daa933d87b2b113fb4758e52b372ea4
David Ericson <davidestudofcg@gmail.com>
2024-11-08 16:18:50 -0300

    feat(Battle): Basic combat and winning structure.

--- src/Battle.java
+++ src/Battle.java
@@ -1,9 +1,21 @@
-
 public class Battle {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void Fight(Element e1, Element e2) {
+		Element attacking = e1;
+		Element deffending = e2;
+		Element aux;
+		
+		int dmg, def, impact, turn=1;
+		
+		do {
+			
+		} while (e1.getLife() > 0 && e2.getLife()> 0);
+		
+		if (e1.getLife() <= 0)
+			System.out.println(e2.getName() + "Won the battle!");
+		else
+			System.out.println(e1.getName() + "Won the battle!");
 
 	}
+	
 
 }
