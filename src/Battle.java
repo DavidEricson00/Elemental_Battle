ca78d0a6843817d0de1449f3d26f1667ff9b524b
David Ericson <davidestudofcg@gmail.com>
2024-11-08 19:05:48 -0300

    fix(Battle): "dmg = attacking.getAtk();" and print without space fix.ed

--- src/Battle.java
+++ src/Battle.java
@@ -1,4 +1,5 @@
 public class Battle {
+	
 	public void Fight(Element e1, Element e2) {
 		Element attacking = e1;
 		Element defending = e2;
@@ -7,23 +8,23 @@ public class Battle {
 		int dmg, def, impact, turn=1;
 		
 		do {
-			dmg = attacking.getAtk();
+			dmg = attacking.Attack();
 			def = defending.Defend();
 			impact = (dmg - def);
 			
 			if (dmg > def) {
-				System.out.println(attacking.getName() + "deals" + impact + "damege to" + defending.getName());
+				System.out.println(attacking.getName() + " deals " + impact + " damege to " + defending.getName());
 				defending.Damage(impact);
 			}
 			else
-				System.out.println(defending.getName() + "deffended the attack");
+				System.out.println(defending.getName() + " deffended the attack ");
 			
 		} while (e1.getLife() > 0 && e2.getLife()> 0);
 		
 		if (e1.getLife() <= 0)
-			System.out.println(e2.getName() + "Won the battle!");
+			System.out.println(e2.getName() + " Won the battle!");
 		else
-			System.out.println(e1.getName() + "Won the battle!");
+			System.out.println(e1.getName() + " Won the battle!");
 
 	}
 	
