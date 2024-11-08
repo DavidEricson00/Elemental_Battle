869b570213898f2f76ee8e19fe867ccb012d688b
David Ericson <davidestudofcg@gmail.com>
2024-11-08 18:51:38 -0300

    feat(Battle): added attack and defend system.

--- src/Battle.java
+++ src/Battle.java
@@ -1,12 +1,22 @@
 public class Battle {
 	public void Fight(Element e1, Element e2) {
 		Element attacking = e1;
-		Element deffending = e2;
+		Element defending = e2;
 		Element aux;
 		
 		int dmg, def, impact, turn=1;
 		
 		do {
+			dmg = attacking.getAtk();
+			def = defending.Defend();
+			impact = (dmg - def);
+			
+			if (dmg > def) {
+				System.out.println(attacking.getName() + "deals" + impact + "damege to" + defending.getName());
+				defending.Damage(impact);
+			}
+			else
+				System.out.println(defending.getName() + "deffended the attack");
 			
 		} while (e1.getLife() > 0 && e2.getLife()> 0);
 		
