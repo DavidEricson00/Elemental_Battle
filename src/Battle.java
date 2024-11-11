e77b93e4d9a452d86185ebd60ac00fb277722620
David Ericson <davidestudofcg@gmail.com>
2024-11-11 11:21:42 -0300

    feat(Battle): added display to battle; now elements, along with their attacks and blocks, appear in the console

--- src/Battle.java
+++ src/Battle.java
@@ -1,33 +1,73 @@
 public class Battle {
 
     public void Fight(Element e1, Element e2) {
+        Display display = new Display();
         Element attacking = e1;
         Element defending = e2;
         Element aux;
+
+        String[] imageE1 = display.getImage1("fire");
+        String[] imageE2 = display.getImage2("water");
+
+        String[] atkE1 = display.getAttack("fire");
+        String[] atkE2 = display.getAttack("water");
+
+        String[] block = display.Block();
+        String[] empty = display.Empty();
 
         int dmg, def, impact, turn = 1;
 
         do {
             System.out.println("---------------------------------------");
-        	System.out.println("Turn " + (turn++));
+            System.out.println("Turn " + (turn++));
             dmg = attacking.Attack();
             def = defending.Defend();
-            
+
             System.out.println(attacking.getName() + " Attacking with: " + dmg);
             System.out.println(defending.getName() + " Defending with: " + def);
-            
-            if (def > dmg)
-            	System.out.println(defending.getName() + " defended the attack!");
-            else {
-            	impact = Math.abs(dmg - def);
-            	System.out.println(attacking.getName() + " deals " + impact + " damage to " + defending.getName());
-            	defending.Damage(impact);
+
+            if (def > dmg) {
+            	if (turn % 2 == 0) {
+	                for (int i = 0; i < 7; i++) {
+	                    System.out.print(imageE1[i]);
+	                    System.out.print(block[i]);
+	                    System.out.print(atkE2[i]);
+	                    System.out.println(imageE2[i]);
+	                }
+                } else {
+	                for (int i = 0; i < 7; i++) {
+	                    System.out.print(imageE1[i]);
+	                    System.out.print(atkE1[i]);
+	                    System.out.print(block[i]);
+	                    System.out.println(imageE2[i]);
+	                }
+                }
+                System.out.println(defending.getName() + " defended the attack!");
+            } else {
+                impact = Math.abs(dmg - def);
+            	if (turn % 2 == 0) {
+	                for (int i = 0; i < 7; i++) {
+	                    System.out.print(imageE1[i]);
+	                    System.out.print(empty[i]);
+	                    System.out.print(atkE2[i]);
+	                    System.out.println(imageE2[i]);
+	                }
+                } else {
+	                for (int i = 0; i < 7; i++) {
+	                    System.out.print(imageE1[i]);
+	                    System.out.print(atkE1[i]);
+	                    System.out.print(empty[i]);
+	                    System.out.println(imageE2[i]);
+	                }
+                }
+                System.out.println(attacking.getName() + " deals " + impact + " damage to " + defending.getName());
+                defending.Damage(impact);
             }
 
             aux = attacking;
             attacking = defending;
             defending = aux;
-            
+
             System.out.println(e1.getName() + " Life: " + e1.getLife());
             System.out.println(e2.getName() + " Life: " + e2.getLife());
 
